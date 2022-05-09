4c4627c790912df299bafbb2bfd791006f57b090
Kristians Rasins <rasziins@gmail.com>
2022-05-09 16:41:17 +0300

    day2

--- src/cepums.java
+++ src/cepums.java
@@ -1,13 +1,12 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.TimerTask;
+import java.util.Timer;
 
 import javax.lang.model.util.ElementScanner14;
 import javax.swing.*;
 
 public class cepums {
-    static int cepumi = 0;
-    static int cepumiplus = 1;
-    static int cena1 = 20;
 
     public static void main(String[] args) {
 
@@ -24,48 +23,73 @@ public class cepums {
         nauda.setFont(new Font("cepumuteksts", Font.BOLD, 35));
         f.add(nauda);
 
+        // Taimeris veic darbibu katru sekundi
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                nauda.setText("cepumi:" + mainigie.cepumi);
+            }
+        }, 1000, 1000);
+
         // GALVENA CEPUMA POGA UN VISAS PAREJAS VEIKALA POGAS
         JButton b = new JButton(new ImageIcon("cepumabilde.png")); // galvenais cepums
-        JButton bplus = new JButton("+1 par klikšķi (" + cena1 + "c)"); // klikšķa plusošana
-        bplus.setBounds(900, 100, 200, 30);
+        JButton bplus = new JButton("+1 par klikšķi (" + mainigie.cena1 + "c)"); // klikšķa plusošana
+        JButton b1 = new JButton("+1 cepums sekunde (" + mainigie.cena2 + "c)"); // 1. cps upgrade
         b.setBounds(0, 0, 500, 500);
         b.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { // action listeners kurs pieskaita cepumus par katru klikšķi
-                cepumi = cepumi + cepumiplus;
-                nauda.setText("cepumi:" + cepumi);
+                mainigie.cepumi = mainigie.cepumi + mainigie.cepumiplus;
+                nauda.setText("cepumi:" + mainigie.cepumi);
                 if (b.isSelected()) {
-                    cepumi = cepumi + cepumiplus;
-                    nauda.setText("cepumi:" + cepumi);
+                    mainigie.cepumi = mainigie.cepumi + mainigie.cepumiplus;
+                    nauda.setText("cepumi:" + mainigie.cepumi);
                 }
             }
         });
+        bplus.setBounds(900, 100, 200, 30);
         bplus.addActionListener(new ActionListener() { // action listeners klikšķa plusošanas pogai
             public void actionPerformed(ActionEvent e) {
-                if (cepumi >= cena1) {
-                    cepumiplus++;
-                    cepumi = cepumi - cena1;
-                    cena1 = cena1 * 3;
-                    bplus.setText("+1 par klikšķi (" + cena1 + "c)");
-                    nauda.setText("cepumi:" + cepumi);
+                if (mainigie.cepumi >= mainigie.cena1) {
+                    mainigie.cepumiplus++;
+                    mainigie.cepumi = mainigie.cepumi - mainigie.cena1;
+                    mainigie.cena1 = mainigie.cena1 * 3;
+                    bplus.setText("+1 par klikšķi (" + mainigie.cena1 + "c)");
+                    nauda.setText("cepumi:" + mainigie.cepumi);
+                }
+
+            }
+        });
+        b1.setBounds(900, 150, 200, 30);
+        b1.addActionListener(new ActionListener() { // action listeners 1 cps upgrade
+            public void actionPerformed(ActionEvent e) {
+                if (mainigie.cepumi >= mainigie.cena2) {
+                    mainigie.cps++;
+                    mainigie.cepumi = mainigie.cepumi - mainigie.cena2;
+                    mainigie.cena2 = mainigie.cena2 * 2;
+                    b1.setText("+1 cepums/sec (" + mainigie.cena2 + "c)");
+                    nauda.setText("cepumi:" + mainigie.cepumi);
 
                 }
             }
         });
+
         f.setSize(400, 400);
         f.setLayout(null);
         f.setVisible(true);
         f.add(nauda);
         f.add(b);
         f.add(bplus);
+        f.add(b1);
 
     };
 
+    // public static boolean enoughcookies(int currentcookies, int requiredcookies){
+    // if (currentcookies>=requiredcookies) {
+    // return true;
+    // } else {
+    // return false;
+    // }
 
-public static boolean enoughcookies(int currentcookies, int requiredcookies){
-    if (currentcookies>=requiredcookies) {
-        return true;
-    } else {
-        return false;
-    }
-        
-}}
+}
---
+++ src/mainigie.java
@@ -0,0 +1,8 @@
+
+public class mainigie {
+    public static int cepumi = 400;
+    public static int cepumiplus = 1;
+    public static int cps = 0;
+    public static int cena1 = 20;
+    public static int cena2 = 300;
+}
